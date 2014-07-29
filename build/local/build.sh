c1a4a5a8d09d0a35c11621846b55f3f0a66eac41
Carla Dexie Amante <carla@promethost.com>
2014-07-29 16:33:40 +0800

    added new content type and added ckeditor and fix build

--- build/local/build.sh
+++ build/local/build.sh
@@ -2,11 +2,17 @@
 echo "Dropping database.";
 $drush sql-drop -y
 echo "Installing database.";
-$drush sqlc -y < $env_path/ref_db/drupalcebuDB.sql
+# $drush sqlc -y < $env_path/ref_db/drupalcebuDB.sql
+echo "Installing database.";
+$drush si standard -y --account-pass='drupaladm1n'
+echo "Clearing registry.";
+$drush cc all -y
+echo "Disabling modules not needed for local development.";
+$drush dis $(cat $env_path/mods_purge | tr '\n' ' ') -y -v
+$drush pm-uninstall $(cat $env_path/mods_purge | tr '\n' ' ') -y -v
+$drush updb -y
 echo "Enabling modules needed for local development.";
 $drush en $(cat $env_path/mods_enabled | tr '\n' ' ') -y -v
-echo "Setting password for local";
-$drush upwd admin --password=drupaladm1n -y
 echo "Clearing caches.";
 $drush cc all -y
 echo "Disabling css and js caching.";
